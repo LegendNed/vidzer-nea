b64e302680d84750f3c329351bfb0d24e615a0f6
Ned <nedasstockus@gmail.com>
2021-12-06 15:23:24 +0000

    updated database connect

--- src/handlers/database.ts
+++ src/handlers/database.ts
@@ -6,23 +6,20 @@ export const connect = async () => {
     let username: string = env ? process.env.DEV_DB_USERNAME : process.env.DB_USERNAME
     let password: string = env ? process.env.DEV_DB_PASSWORD : process.env.DB_PASSWORD
 
+    mongoose.set('debug', env) //Needed somehow lol
+    let database: any = await mongoose
+        .connect(url, {
+            useUnifiedTopology: true,
+            useNewUrlParser: true,
+            loggerLevel: env ? 'debug' : null, //Enable debugging when developing
+            user: username,
+            pass: password,
+        } as ConnectOptions)
 
-    return new Promise(async (resolve: (arg0: void) => void) => {
-        mongoose.set('debug', env)
-        await mongoose
-            .connect(url, {
-                useUnifiedTopology: true,
-                useNewUrlParser: true,
-                loggerLevel: env ? 'debug' : null, //Enable debugging when developing
-                user: username,
-                pass: password,
-            } as ConnectOptions)
-
-
-        mongoose.connection.on('connecting', () => console.info('database connecting'))
-        mongoose.connection.on('connected', () => resolve(console.info('database connected')))
-        mongoose.connection.on('disconnecting', () => console.info('database disconnecting'))
-        mongoose.connection.on('disconnected', () => console.info('database disconnected'))
-        mongoose.connection.on('error', () => console.error('database error'))
-    })
+    let connection = database.connections[0]
+    connection.on('connecting', () => console.info('database connecting'))
+    connection.on('connected', () => console.info('database connected'))
+    connection.on('disconnecting', () => console.info('database disconnecting'))
+    connection.on('disconnected', () => console.info('database disconnected'))
+    connection.on('error', () => console.error('database error'))
 }
